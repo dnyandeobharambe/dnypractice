d278937ab9ed46010a8ee9538a660f7d2deea8d0
dnyandeobharambe <39019068+dnyandeobharambe@users.noreply.github.com>
2019-04-02 15:06:27 -0700

    Add files via upload

---
+++ MathOPUsingBiwise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathOPUsingBiwise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
