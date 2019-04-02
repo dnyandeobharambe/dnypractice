5cbc9df7be776ec11ada0fafa36041b2c7ec35da
dnyandeobharambe <39019068+dnyandeobharambe@users.noreply.github.com>
2019-04-02 15:03:43 -0700

    Add files via upload

---
+++ CharFrequency/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CharFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Break dtring to character
+            //Add characters in dictionary
+            //dictionary does not allow duplicate, so when key exist increase count
+            // Value of dictionary indicates frequency
+            // initialize max value as count when count is bigger than max, replace max with new value
+            //final max value will be highest frequency 
+            Char maxFreqAns = GetMaxFrequency("abacdabaefacabbbbccccccccccccccccccccccccccc");
+            Console.WriteLine("Max Freq Char is " + maxFreqAns);
+            Console.ReadLine();
+
+            //try remove duplicate and pallindrom permutation 
+        }
+
+        public static char GetMaxFrequency(string s)
+        {
+            Dictionary<char, int> stringDictionary = new Dictionary<char, int>();
+            int max = 1;
+            char ans  = '|';
+
+            foreach (char c in s.ToCharArray())
+            {
+              
+                if (stringDictionary.ContainsKey(c))
+                {
+             
+                    stringDictionary[c]++;
+                    if(stringDictionary[c]++ > max)
+                    {
+                        max = stringDictionary[c]++;
+                        ans = c;
+                    }
+                }
+                else
+                {
+                    stringDictionary.Add(c, 1);
+                }
+                //i++;
+            }
+
+            return ans;
+
+        }
+    }
+}
