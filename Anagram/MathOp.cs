6a147cf26e729e809ada686e1867c7fdeec4234e
dnyandeobharambe <39019068+dnyandeobharambe@users.noreply.github.com>
2019-04-02 14:59:41 -0700

    Add files via upload

---
+++ Anagram/MathOp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anagram
+{
+    //note << is euvalant to multiply by 2 say 5<<2 means 5*2powerof2 that is 5*4
+    //>> is divide by 2
+    public class MathOp
+    {
+
+        public int Add(int x,int y)
+        {
+            while(y!= 0)
+            {
+                int carry = x & y;
+                 x = x ^ y;
+                y = carry << 1;
+            }
+
+            return x;
+        }
+
+        public int Substract(int x, int y)
+        {
+            while (y != 0)
+            {
+                int borrow = (~x) & y;
+                x = x ^ y;
+                y = borrow << 1;
+            }
+
+            return x;
+        }
+
+        public int Multiply(int x,int y)
+        {
+            if (y == 0)
+                return 0;
+            if (y > 0)
+                return x + Multiply(x, y - 1);
+            if (y < 0)
+                return -Multiply(x, -y);
+            return -1;
+        }
+
+        public int Division(int x,int y)
+        {
+            int q = 0;
+            while(x >= y)
+            {
+                x = x - y;
+                q++;
+            }
+
+            return q;
+
+        }
+
+        //public int Product(int x,int y)
+        //{
+        //    return x + (x << 1);
+        //}
+    }
+}
---
+++ Anagram/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anagram
+{
+
+    //Create directory (characters as key and number of times character come as value) by characters of first string
+    //Create directory of characters of second string
+    //Compare two directories if key or value not mtaching means not anagram
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean ans = isAnagram("dnyandeo", "nyandeob");
+            //Console.Write("Answer is" + ans.ToString());
+
+            MathOp mathop = new MathOp();
+            //int ans = mathop.Add(-12,-13);
+            //int ans = mathop.Substract(50, 25);
+            //int ans = mathop.Multiply(50, 25);
+            int ans = mathop.Division(13, 2);
+            Console.WriteLine("Answer is :" + ans);
+            Console.ReadLine();
+
+
+        }
+
+        private static Boolean  isAnagram(string first,string second)
+        {
+            if(first == second)
+            {
+                return true;
+            }
+            else
+            {
+                if(first.Length != second.Length )
+                {
+                    return false;
+                }
+                else
+                { 
+
+                    Dictionary<char,int> firstDictionary = new Dictionary<char, int>();
+                    Dictionary<char, int> secondDictionary = new Dictionary<char, int>();
+
+                    foreach (char c in first.ToCharArray())
+                    {
+                        int i = 1;
+                         if(firstDictionary.ContainsKey(c))
+                        {
+                            firstDictionary[c]++;
+                        }
+                         else
+                        { 
+                        firstDictionary.Add(c, i);
+                        }
+                        //i++;
+                    }
+
+                    foreach (char c in second.ToCharArray())
+                    {
+                        int i = 1;
+                        if (secondDictionary.ContainsKey(c))
+                        {
+                            secondDictionary[c]++;
+                        }
+                        else
+                        {
+                            secondDictionary.Add(c, i);
+                        }
+                        //i++;
+                    }
+
+                    foreach(var k in firstDictionary)
+                    {
+                        if(!secondDictionary.ContainsKey(k.Key))
+                        {
+                            return false;
+                        }
+                        if(firstDictionary[k.Key] != secondDictionary[k.Key])
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+
+
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
